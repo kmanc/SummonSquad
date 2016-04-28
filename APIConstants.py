21b2d27fa1ec577a2058bc2f5cdeb3b742f94e2b
Kevin <kmanc@comcast.net>
2016-04-28 18:06:37 -0400

    started to expand api functionality, but i need to pause to figure out what the goal is

--- APIConstants.py
+++ APIConstants.py
@@ -1,12 +1,22 @@
 URL = {
     'base': 'https://{proxy}.api.pvp.net/api/lol/{region}/{url}',
-    'summoner_by_name': 'v{version}/summoner/by-name/{names}'
+    'summoner_by_name': 'v{version}/summoner/by-name/{names}',
+    'mastery_base':'https://{proxy}.api.pvp.net/',
+    'all_mastery':'championmastery/location/{platformId}/player/{playerId}/champions',
+    'mastery_by_champ':'championmastery/location/{platformId}/player/{playerId}/champion/{championId}',
 }
 
 API_VERSIONS = {
+    'champion': '1.2',
+    'championmastery': '',
+    'stats': '1.3',
     'summoner': '1.4'
 }
 
 REGIONS = {
     'north_america': 'na'
+}
+
+PLATFORMS = {
+    'north_america': 'NA1'
 }
--- RiotAPI.py
+++ RiotAPI.py
@@ -20,7 +20,21 @@ class RiotAPI(object):
             ),
             params=args
         )
-        print (response.url)
+        #print (response.url)
+        return response.json()
+        
+    def _requestTwo(self, api_url, params={}):
+        args = {'api_key': self.api_key}
+        for key, value in params.items():
+            if key not in args:
+                args[key] = value
+        response = requests.get(
+            Constants.URL['mastery_base'].format(
+                proxy=self.region
+            ),
+            params=args
+        )
+        #print (response.url)
         return response.json()
         
     def get_summoner_by_name(self, name):
@@ -29,4 +43,12 @@ class RiotAPI(object):
             names=name
         )
         return self._request(api_url)
+       
+    def get_summoner_mastery_data(self, id):
+        api_url = Constants.URL['all_mastery'].format(
+            platformId=Constants.API_PLATFORMS['north_america'],
+            playerId=id
+        )
+        return self._requestTwo(api_url)
+     
         
