742f4cb4f75efe696cac928c3bee6472aaf3dfd8
Kevin <kmanc@comcast.net>
2016-05-01 21:25:51 -0400

    i now have a list of champion ids and their corresponsing mastery score for a given summoner

--- Main.py
+++ Main.py
@@ -4,29 +4,30 @@ import json
 def main():
     api = RiotAPI('e6e27f9f-d47d-4b3f-96d4-3aab2c2b8cee')
     
+    
     # Get summoner data
     response = api.get_summoner_by_name('KmancXC')
-    print (response)
     
     # Parse the output
     temp = json.dumps(response)
     temp = json.loads(temp)
-    print (type(response))
-    print (type(temp))
     summoner_id = (temp['kmancxc']['id'])
     summoner_name = (temp['kmancxc']['name'])
+    
+    #For troubleshooting
     print (summoner_id, summoner_name)
     
-    # Get summoner's mastery data
+    # Get all of a summoner's mastery data
     #response = api.get_summoner_mastery_data(summoner_id)
     #print (response)
     
-    # Get summoner's top X mastery data
+    # Get a summoner's top X mastery data
     champs = 10
-    i = 0
+    data_dict = {}
     response = api.get_top_mastery_data(summoner_id, champs)
-    print (type(response))
-    print (response)
+    for x, val in enumerate(response):
+        data_dict[val['championId']] = val['championPoints']
+    print (data_dict)
 
 if __name__ == '__main__':
     main()
