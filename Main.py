217178317dc6396fd376e31acde94266b882a807
Kevin <kmanc@comcast.net>
2016-05-05 18:00:45 -0400

    i can now access the role of every champion played in the summonerId object

--- Main.py
+++ Main.py
@@ -5,17 +5,26 @@ from GetData import GetData
 
 def main():
     
-    summoners = ['Kmancxc','berkfleriosa']
+    summoners = ['Kmancxc', 'berkfleriosa']
     for x in range(len(summoners)):
         summoners[x] = summoners[x].lower()
 
-    myList = []
+    summonerList = []
     data_grabber = GetData()
     for player in summoners:
         summoner_data = data_grabber._gimme_data(player)
-        myList.append(summoner_data)
-    for x in range(len(myList)):
-        print (myList[x])
+        summonerList.append(summoner_data)
+    
+    for summoner in range(len(summonerList)):
+        #print (summonerList[summoner])
+        for x, mastered_champs in summonerList[summoner].items():
+            #print (mastered_champs)
+            for x, champ_info in mastered_champs.items():
+                #print (champ_info[1:])
+                for role_info in champ_info[1:]:
+                    #print (role_info)
+                    for role in role_info:
+                        print (role)
     
 if __name__ == '__main__':
     main()
