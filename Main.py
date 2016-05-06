74059ff954a8726d9ba51e96a578be8dcf05bf66
Kevin <kmanc@comcast.net>
2016-05-05 21:01:10 -0400

    fixed the issue of iterating through all possible teams.  the algorithm now returns all 'valid' teams

--- Main.py
+++ Main.py
@@ -26,8 +26,14 @@ def main():
                     #print (role_info0)
                     for role0 in role_info0:
                         #print ('summoner 0', x0, role0)
+                        roleList = []
+                        champList = []
                         summoner_zero_role = role0
                         summoner_zero_champ = x0
+                        if (summoner_zero_role == 'NONE' or summoner_zero_role == 'DUO'):
+                            continue
+                        roleList.append(summoner_zero_role)
+                        champList.append(summoner_zero_champ)
                         for x1, mastered_champs1 in summonerList[1].items():
                             #print (mastered_champs1)
                             for x1, champ_info1 in mastered_champs1.items():
@@ -38,6 +44,11 @@ def main():
                                         #print ('summoner 1', x1, role1)
                                         summoner_one_role = role1
                                         summoner_one_champ = x1
+                                        if (summoner_one_role == 'NONE' or summoner_one_role == 'DUO' or summoner_one_role in roleList or len(roleList) == 0 or summoner_one_champ in champList):
+                                            continue
+                                        roleList.append(summoner_one_role)
+                                        champList.append(summoner_one_champ)
+                                        #print (roleList)
                                         for x2, mastered_champs2 in summonerList[2].items():
                                             #print (mastered_champs2)
                                             for x2, champ_info2 in mastered_champs2.items():
@@ -48,6 +59,10 @@ def main():
                                                         #print ('summoner 2', x2, role2)
                                                         summoner_two_role = role2
                                                         summoner_two_champ = x2
+                                                        if (summoner_two_role == 'NONE' or summoner_two_role == 'DUO' or summoner_two_role in roleList or len(roleList) <= 1 or summoner_two_champ in champList):
+                                                            continue
+                                                        roleList.append(summoner_two_role)
+                                                        champList.append(summoner_two_champ)
                                                         for x3, mastered_champs3 in summonerList[3].items():
                                                             #print (mastered_champs3)
                                                             for x3, champ_info3 in mastered_champs3.items():
@@ -58,6 +73,10 @@ def main():
                                                                         #print ('summoner 3', x3, role3)
                                                                         summoner_three_role = role3
                                                                         summoner_three_champ = x3
+                                                                        if (summoner_three_role == 'NONE' or summoner_three_role == 'DUO' or summoner_three_role in roleList or len(roleList) <= 2 or summoner_three_champ in champList):
+                                                                            continue
+                                                                        champList.append(summoner_three_champ)
+                                                                        roleList.append(summoner_three_role)
                                                                         for x4, mastered_champs4 in summonerList[4].items():
                                                                             #print (mastered_champs4)
                                                                             for x4, champ_info4 in mastered_champs4.items():
@@ -65,9 +84,20 @@ def main():
                                                                                 for role_info4 in champ_info4[1:]:
                                                                                     #print (role_info4)
                                                                                     for role4 in role_info4:
-                                                                                        print ('summoner 4', x4, role4)
+                                                                                        #print ('summoner 4', x4, role4)
                                                                                         summoner_four_role = role4
                                                                                         summoner_four_champ = x4
+                                                                                        if (summoner_four_role == 'NONE' or summoner_four_role == 'DUO' or summoner_four_role in roleList or len(roleList) <= 3 or summoner_four_champ in champList):
+                                                                                            continue
+                                                                                        else:
+                                                                                            roleList = []
+                                                                                            print ('valid team found')
+                                                                                            print (summoner_zero_champ, summoner_zero_role)
+                                                                                            print (summoner_one_champ, summoner_one_role)
+                                                                                            print (summoner_two_champ, summoner_two_role)
+                                                                                            print (summoner_three_champ, summoner_three_role)
+                                                                                            print (summoner_four_champ, summoner_four_role)
+                                                                                            print ()
     
     
 if __name__ == '__main__':
