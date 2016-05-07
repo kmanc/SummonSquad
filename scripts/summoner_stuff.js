cf50d06f721461f3f84d620550599da5e2a4692b
Joshua Semedo <joshsemedo@gmail.com>
2016-05-07 12:59:43 -0400

    updating UI

--- scripts/summoner_stuff.js
+++ scripts/summoner_stuff.js
@@ -1,282 +1,153 @@
 	$(document).ready(function(){
 	    $("#s1").mouseenter(function(){
 	   		$('#s1').css({
-				'font-family': 'Gentium Basic',
-				'display': 'block',
-				'padding': 0,
-				'width': 300,
-				'height': 50,
-				'margin':'auto',
-				'background-color': 'transparent',
 				'border-color': '#40a0a5',
-				'border' : 1,
 				'border-radius': 1,
-				'border-right-style': 'solid',
 				'border-right-width': 0,
-				'border-left-style': 'solid',
 				'border-left-width': 0,
-				'border-top-style': 'solid',
-				'border-top-width': 0,
-				'border-bottom-style': 'solid',
-				'border-bottom-width': 0,
-				'text-align': 'center',
-				'font-size': 24,
-				'transition-duration': '.3s' 
+				'border-top-width': 1,
+				'border-bottom-width': 1,
 		    });
 		}); 
 	    $('#s1').mouseleave(function(){
 	    	$('#s1').css({
-				'font-family': 'Gentium Basic',
-				'display': 'block',
-				'padding': 0,
-				'width': 300,
-				'height': 50,
-				'margin':'auto',
-				'background-color': 'transparent',
 				'border-color': 'rgba(20, 91, 95, .5)',
 				'border-radius': 1,
-				'border-right-style': 'solid',
-				'border-right-width': 0,
-				'border-left-style': 'solid',
-				'border-left-width': 0,
-				'border-top-style': 'solid',
-				'border-top-width': '26',
-				'border-bottom-style': 'solid',
-				'border-bottom-width': '25',
-				'text-align': 'center',
-				'font-size': 24,
-				'transition-duration': '.3s'
+				'border-right-width': '2',
+				'border-left-width': '2',
+				'border-top-width': '2',
+				'border-bottom-width': '2',
 			});
 	    });
 	    $("#s2").mouseenter(function(){
 	   		$('#s2').css({
-				'font-family': 'Gentium Basic',
-				'display': 'block',
-				'padding': 0,
-				'width': 300,
-				'height': 50,
-				'margin':'auto',
-				'background-color': 'transparent',
 				'border-color': '#40a0a5',
-				'border' : 1,
 				'border-radius': 1,
-				'border-right-style': 'solid',
 				'border-right-width': 0,
-				'border-left-style': 'solid',
 				'border-left-width': 0,
-				'border-top-style': 'solid',
-				'border-top-width': 0,
-				'border-bottom-style': 'solid',
-				'border-bottom-width': 0,
-				'text-align': 'center',
-				'font-size': 24,
-				'transition-duration': '.3s' 
+				'border-top-width': 1,
+				'border-bottom-width': 1,
 		    });
 		}); 
 	    $('#s2').mouseleave(function(){
 	    	$('#s2').css({
-				'font-family': 'Gentium Basic',
-				'display': 'block',
-				'padding': 0,
-				'width': 300,
-				'height': 50,
-				'margin':'auto',
-				'background-color': 'transparent',
 				'border-color': 'rgba(20, 91, 95, .5)',
 				'border-radius': 1,
-				'border-right-style': 'solid',
-				'border-right-width': 0,
-				'border-left-style': 'solid',
-				'border-left-width': 0,
-				'border-top-style': 'solid',
-				'border-top-width': '26',
-				'border-bottom-style': 'solid',
-				'border-bottom-width': '25',
-				'text-align': 'center',
-				'font-size': 24,
-				'transition-duration': '.3s'
+				'border-right-width': '2',
+				'border-left-width': '2',
+				'border-top-width': '2',
+				'border-bottom-width': '2',
 			});
 	    });
 	    $("#s3").mouseenter(function(){
 	   		$('#s3').css({
-				'font-family': 'Gentium Basic',
-				'display': 'block',
-				'padding': 0,
-				'width': 300,
-				'height': 50,
-				'margin':'auto',
-				'background-color': 'transparent',
 				'border-color': '#40a0a5',
-				'border' : 1,
 				'border-radius': 1,
-				'border-right-style': 'solid',
 				'border-right-width': 0,
-				'border-left-style': 'solid',
 				'border-left-width': 0,
-				'border-top-style': 'solid',
-				'border-top-width': 0,
-				'border-bottom-style': 'solid',
-				'border-bottom-width': 0,
-				'text-align': 'center',
-				'font-size': 24,
-				'transition-duration': '.3s' 
+				'border-top-width': 1,
+				'border-bottom-width': 1,
 		    });
 		}); 
 	    $('#s3').mouseleave(function(){
 	    	$('#s3').css({
-				'font-family': 'Gentium Basic',
-				'display': 'block',
-				'padding': 0,
-				'width': 300,
-				'height': 50,
-				'margin':'auto',
-				'background-color': 'transparent',
 				'border-color': 'rgba(20, 91, 95, .5)',
 				'border-radius': 1,
-				'border-right-style': 'solid',
-				'border-right-width': 0,
-				'border-left-style': 'solid',
-				'border-left-width': 0,
-				'border-top-style': 'solid',
-				'border-top-width': '26',
-				'border-bottom-style': 'solid',
-				'border-bottom-width': '25',
-				'text-align': 'center',
-				'font-size': 24,
-				'transition-duration': '.3s'
+				'border-right-width': '2',
+				'border-left-width': '2',
+				'border-top-width': '2',
+				'border-bottom-width': '2',
 			});
 	    });
 	    $("#s4").mouseenter(function(){
 	   		$('#s4').css({
-				'font-family': 'Gentium Basic',
-				'display': 'block',
-				'padding': 0,
-				'width': 300,
-				'height': 50,
-				'margin':'auto',
-				'background-color': 'transparent',
 				'border-color': '#40a0a5',
-				'border' : 1,
 				'border-radius': 1,
-				'border-right-style': 'solid',
 				'border-right-width': 0,
-				'border-left-style': 'solid',
 				'border-left-width': 0,
-				'border-top-style': 'solid',
-				'border-top-width': 0,
-				'border-bottom-style': 'solid',
-				'border-bottom-width': 0,
-				'text-align': 'center',
-				'font-size': 24,
-				'transition-duration': '.3s' 
+				'border-top-width': 1,
+				'border-bottom-width': 1,
 		    });
 		}); 
 	    $('#s4').mouseleave(function(){
 	    	$('#s4').css({
-				'font-family': 'Gentium Basic',
-				'display': 'block',
-				'padding': 0,
-				'width': 300,
-				'height': 50,
-				'margin':'auto',
-				'background-color': 'transparent',
 				'border-color': 'rgba(20, 91, 95, .5)',
 				'border-radius': 1,
-				'border-right-style': 'solid',
-				'border-right-width': 0,
-				'border-left-style': 'solid',
-				'border-left-width': 0,
-				'border-top-style': 'solid',
-				'border-top-width': '26',
-				'border-bottom-style': 'solid',
-				'border-bottom-width': '25',
-				'text-align': 'center',
-				'font-size': 24,
-				'transition-duration': '.3s'
+				'border-right-width': '2',
+				'border-left-width': '2',
+				'border-top-width': '2',
+				'border-bottom-width': '2',
 			});
 	    });
 	    $("#s5").mouseenter(function(){
 	   		$('#s5').css({
-				'font-family': 'Gentium Basic',
-				'display': 'block',
-				'padding': 0,
-				'width': 300,
-				'height': 50,
-				'margin':'auto',
-				'background-color': 'transparent',
 				'border-color': '#40a0a5',
-				'border' : 1,
 				'border-radius': 1,
-				'border-right-style': 'solid',
 				'border-right-width': 0,
-				'border-left-style': 'solid',
 				'border-left-width': 0,
-				'border-top-style': 'solid',
-				'border-top-width': 0,
-				'border-bottom-style': 'solid',
-				'border-bottom-width': 0,
-				'text-align': 'center',
-				'font-size': 24,
-				'transition-duration': '.3s' 
+				'border-top-width': 1,
+				'border-bottom-width': 1,
 		    });
 		}); 
 	    $('#s5').mouseleave(function(){
 	    	$('#s5').css({
-				'font-family': 'Gentium Basic',
-				'display': 'block',
-				'padding': 0,
-				'width': 300,
-				'height': 50,
-				'margin':'auto',
-				'background-color': 'transparent',
 				'border-color': 'rgba(20, 91, 95, .5)',
-				'border-radius': 0,
-				'border-right-style': 'solid',
-				'border-right-width': 0,
-				'border-left-style': 'solid',
-				'border-left-width': 0,
-				'border-top-style': 'solid',
-				'border-top-width': '26',
-				'border-bottom-style': 'solid',
-				'border-bottom-width': '25',
-				'text-align': 'center',
-				'font-size': 24,
-				'transition-duration': '.3s'
+				'border-radius': 1,
+				'border-right-width': '2',
+				'border-left-width': '2',
+				'border-top-width': '2',
+				'border-bottom-width': '2',
 			});
 	    });
 
-	    $('div#summ1').hover(function(){
-	   		$('body').css({
-	   			'background-image': 'url("../imgs/teemo.jpg")',
-	   			'background-size': '100% 100%',
+/*	    $('div#summ1').hover(function(){
+	   		$('html').css({
+				'background' : 'url(../imgs/teemo.jpg) no-repeat center center fixed',
+				'-webkit-background-size' : 'cover',
+				'-moz-background-size' : 'cover',
+				'-o-background-size' : 'cover',
+				'background-size' : 'cover',
 				'transition-duration': '.3s' 
 		    });
 		}); 
+*/
 	    $('div#summ2').hover(function(){
-	   		$('body').css({
-	   			'background-image': 'url("../imgs/aatrox.jpg")',
-	   			'background-size': '100% 100%',
+	   		$('html').css({
+				'background' : 'url(../imgs/aatrox.jpg) no-repeat center center fixed',
+				'-webkit-background-size' : 'cover',
+				'-moz-background-size' : 'cover',
+				'-o-background-size' : 'cover',
+				'background-size' : 'cover',
 				'transition-duration': '.3s' 
 		    });
 		}); 
 	    $('div#summ3').hover(function(){
-	   		$('body').css({
-	   			'background-image': 'url("../imgs/leesin.jpg")',
-	   			'background-size': '100% 100%',
+	   		$('html').css({
+				'background' : 'url(../imgs/leesin.jpg) no-repeat center center fixed',
+				'-webkit-background-size' : 'cover',
+				'-moz-background-size' : 'cover',
+				'-o-background-size' : 'cover',
+				'background-size' : 'cover',
 				'transition-duration': '.3s' 
 		    });
 		}); 
 	    $('div#summ4').hover(function(){
-	   		$('body').css({
-	   			'background-image': 'url("../imgs/taric.jpg")',
-	   			'background-size': '100% 100%',
+	   		$('html').css({
+				'background' : 'url(../imgs/taric.jpg) no-repeat center center fixed',
+				'-webkit-background-size' : 'cover',
+				'-moz-background-size' : 'cover',
+				'-o-background-size' : 'cover',
+				'background-size' : 'cover',
 				'transition-duration': '.3s' 
 		    });
 		}); 
 	    $('div#summ5').hover(function(){
-	   		$('body').css({
-	   			'background-image': 'url("../imgs/graves.jpg")',
-	   			'background-size': '100% 100%',
+	   		$('html').css({
+				'background' : 'url(../imgs/graves.jpg) no-repeat center center fixed',
+				'-webkit-background-size' : 'cover',
+				'-moz-background-size' : 'cover',
+				'-o-background-size' : 'cover',
+				'background-size' : 'cover',
 				'transition-duration': '.3s' 
 		    });
 		}); 
--- webpages/summoner_mastery_results.php
+++ webpages/summoner_mastery_results.php
@@ -3,51 +3,256 @@
   <meta charset="utf-8">
   <link href='https://fonts.googleapis.com/css?family=Gentium+Basic:400,700italic' rel='stylesheet' type='text/css'>
   <link href='riot.css' rel='stylesheet' type='text/css'>
-  <title>DreamTeam.gg</title>
+  <title>SummonSquad</title>
 	<script src="../scripts/jquery.js"></script>
-	<script src="../scripts/summoner_stuff.js"></script>
 </head>
-<body style="background-color: black;">
-	<div class="body_wrapper">
-		<h1>DreamTeam.gg</h1>
+<body>
+		<h1>SummonSquad</h1>
+		<h4><?php
+				$sum1sani = str_replace(' ','',$_POST['sum1']);
+				$sum2sani = str_replace(' ','',$_POST['sum2']);
+				$sum3sani = str_replace(' ','',$_POST['sum3']);
+				$sum4sani = str_replace(' ','',$_POST['sum4']);
+				$sum5sani = str_replace(' ','',$_POST['sum5']);
+				$python = '/usr/bin/python3.4 ';
+				$path = '/var/www/html/scripts/Main.py ';
+				$script = "/usr/bin/python3.4 /var/www/html/scripts/Main.py " . $sum1sani . " " . $sum2sani . " " . $sum3sani . " " . $sum4sani . " " . $sum5sani;
+				//kmancxc, apajunky, raker, ashtreelee, iamjerbear
+				$command = escapeshellcmd($script);
+				$results = exec($command, $my_output, $status);
+				$data_array = explode(",", $results);
+				$champ1 = str_replace(array('"', ' '), '' , $data_array[1]);
+				$champ2 = str_replace(array('"', ' '), '' , $data_array[4]);
+				$champ3 = str_replace(array('"', ' '), '' , $data_array[7]);
+				$champ4 = str_replace(array('"', ' '), '' , $data_array[10]);	
+				$champ5 = str_replace(array('"', ' '), '' , $data_array[13]);
+
+				$role1 = ucfirst(strtolower(preg_replace('/DUO_/', '', $data_array[2])));
+				$role2 = ucfirst(strtolower(preg_replace('/DUO_/', '', $data_array[5])));
+				$role3 = ucfirst(strtolower(preg_replace('/DUO_/', '', $data_array[8])));
+				$role4 = ucfirst(strtolower(preg_replace('/DUO_/', '', $data_array[11])));
+				$role5 = ucfirst(strtolower(preg_replace('/DUO_/', '', $data_array[14])));
+				?>
+			 </h4>
 			<div class="summoners">
-				<div id="summ1">
-					<span style="position: absolute; bottom: 0;">
-						<?php
-						echo $_POST['sum1'];
-						echo '<br>Top';
-						?>
-					</span>
-				</div><div id="summ2">
-					<span style="position: absolute; bottom: 0;">
-						<?php
-						echo $_POST['sum2'];
-						echo '<br>Mid';
-						?>
-					</span>
-				</div><div id="summ3">
-					<span style="position: absolute; bottom: 0;">
-						<?php
-						echo $_POST['sum3'];
-						echo '<br>Jungle';
-						?>
-					</span>
-				</div><div id="summ4">
-					<span style="position: absolute; bottom: 0;">
-						<?php
-						echo $_POST['sum4'];
-						echo '<br>Support';
-						?>
-					</span>
-				</div><div id="summ5">
-					<span style="position: absolute; bottom: 0;">
-						<?php
-						echo $_POST['sum5'];
-						echo '<br>ADC';
-						?>
-					</span>
+				<div class="readText">
+					<div id="summ1">
+						<span style="position: absolute; bottom: 0;">
+							<?php
+								echo $_POST['sum1'];
+								echo '<br>' . $data_array[1];
+								echo '<br>' . $role1;
+							?>
+						</span>
+					</div><div id="summ2">
+						<span style="position: absolute; bottom: 0;">
+							<?php
+								echo $_POST['sum2'];
+								echo '<br>' . $data_array[4];
+								echo '<br>' . $role2;
+							?>
+						</span>
+					</div><div id="summ3">
+						<span style="position: absolute; bottom: 0;">
+							<?php
+								echo $_POST['sum3'];
+								echo '<br>' . $data_array[7];
+								echo '<br>' . $role3;
+							?>
+						</span>
+					</div><div id="summ4">
+						<span style="position: absolute; bottom: 0;">
+							<?php
+								echo $_POST['sum4'];
+								echo '<br>' . $data_array[10];
+								echo '<br>' . $role4;
+							?>
+						</span>
+					</div><div id="summ5">
+						<span style="position: absolute; bottom: 0;">
+							<?php
+								echo $_POST['sum5'];
+								echo '<br>' . $data_array[13];
+								echo '<br>' . $role5;
+							?>
+						</span>
+					</div>
 				</div>
 			</div>
-	</div>
+
+<script>
+	$(document).ready(function(){
+	    $("#s1").mouseenter(function(){
+	   		$('#s1').css({
+				'border-color': '#40a0a5',
+				'border-radius': 1,
+				'border-right-width': 0,
+				'border-left-width': 0,
+				'border-top-width': 1,
+				'border-bottom-width': 1,
+		    });
+		}); 
+	    $('#s1').mouseleave(function(){
+	    	$('#s1').css({
+				'border-color': 'rgba(20, 91, 95, .5)',
+				'border-radius': 1,
+				'border-right-width': '2',
+				'border-left-width': '2',
+				'border-top-width': '2',
+				'border-bottom-width': '2',
+			});
+	    });
+	    $("#s2").mouseenter(function(){
+	   		$('#s2').css({
+				'border-color': '#40a0a5',
+				'border-radius': 1,
+				'border-right-width': 0,
+				'border-left-width': 0,
+				'border-top-width': 1,
+				'border-bottom-width': 1,
+		    });
+		}); 
+	    $('#s2').mouseleave(function(){
+	    	$('#s2').css({
+				'border-color': 'rgba(20, 91, 95, .5)',
+				'border-radius': 1,
+				'border-right-width': '2',
+				'border-left-width': '2',
+				'border-top-width': '2',
+				'border-bottom-width': '2',
+			});
+	    });
+	    $("#s3").mouseenter(function(){
+	   		$('#s3').css({
+				'border-color': '#40a0a5',
+				'border-radius': 1,
+				'border-right-width': 0,
+				'border-left-width': 0,
+				'border-top-width': 1,
+				'border-bottom-width': 1,
+		    });
+		}); 
+	    $('#s3').mouseleave(function(){
+	    	$('#s3').css({
+				'border-color': 'rgba(20, 91, 95, .5)',
+				'border-radius': 1,
+				'border-right-width': '2',
+				'border-left-width': '2',
+				'border-top-width': '2',
+				'border-bottom-width': '2',
+			});
+	    });
+	    $("#s4").mouseenter(function(){
+	   		$('#s4').css({
+				'border-color': '#40a0a5',
+				'border-radius': 1,
+				'border-right-width': 0,
+				'border-left-width': 0,
+				'border-top-width': 1,
+				'border-bottom-width': 1,
+		    });
+		}); 
+	    $('#s4').mouseleave(function(){
+	    	$('#s4').css({
+				'border-color': 'rgba(20, 91, 95, .5)',
+				'border-radius': 1,
+				'border-right-width': '2',
+				'border-left-width': '2',
+				'border-top-width': '2',
+				'border-bottom-width': '2',
+			});
+	    });
+	    $("#s5").mouseenter(function(){
+	   		$('#s5').css({
+				'border-color': '#40a0a5',
+				'border-radius': 1,
+				'border-right-width': 0,
+				'border-left-width': 0,
+				'border-top-width': 1,
+				'border-bottom-width': 1,
+		    });
+		}); 
+	    $('#s5').mouseleave(function(){
+	    	$('#s5').css({
+				'border-color': 'rgba(20, 91, 95, .5)',
+				'border-radius': 1,
+				'border-right-width': '2',
+				'border-left-width': '2',
+				'border-top-width': '2',
+				'border-bottom-width': '2',
+			});
+	    });
+
+	    $('div#summ1').hover(function(){
+	   		$('html').css({
+				'background' : 'url(../bg/<?php echo $champ1; ?>.jpg) no-repeat center center fixed',
+				'background-position' : '50% 90%',
+				'-webkit-background-size' : 'cover',
+				'-moz-background-size' : 'cover',
+				'-o-background-size' : 'cover',
+				'background-size' : 'cover',
+				'transition-duration': '.3s' 
+		    });
+		}); 
+
+	    $('div#summ2').hover(function(){
+	   		$('html').css({
+				'background' : 'url(../bg/<?php echo $champ2; ?>.jpg) no-repeat center center fixed',
+				'background-position' : '50% 70%',
+				'-webkit-background-size' : 'cover',
+				'-moz-background-size' : 'cover',
+				'-o-background-size' : 'cover',
+				'background-size' : 'cover',
+				'transition-duration': '.3s' 
+		    });
+		}); 
+	    $('div#summ3').hover(function(){
+	   		$('html').css({
+				'background' : 'url(../bg/<?php echo $champ3; ?>.jpg) no-repeat center center fixed',
+				'background-position' : '50% 50%',
+				'-webkit-background-size' : 'cover',
+				'-moz-background-size' : 'cover',
+				'-o-background-size' : 'cover',
+				'background-size' : 'cover',
+				'transition-duration': '.3s' 
+		    });
+		}); 
+	    $('div#summ4').hover(function(){
+	   		$('html').css({
+				'background' : 'url(../bg/<?php echo $champ4; ?>.jpg) no-repeat center center fixed',
+				'background-position' : '50% 30%',
+				'-webkit-background-size' : 'cover',
+				'-moz-background-size' : 'cover',
+				'-o-background-size' : 'cover',
+				'background-size' : 'cover',
+				'transition-duration': '.3s' 
+		    });
+		}); 
+	    $('div#summ5').hover(function(){
+	   		$('html').css({
+				'background' : 'url(../bg/<?php echo $champ5; ?>.jpg) no-repeat center center fixed',
+				'background-position' : '50% 10%',
+				'-webkit-background-size' : 'cover',
+				'-moz-background-size' : 'cover',
+				'-o-background-size' : 'cover',
+				'background-size' : 'cover',
+				'transition-duration': '.3s' 
+		    });
+		}); 
+	});
+	$(window).load(function(){
+   		$('html').css({
+			'background' : 'url(../bg/<?php echo $champ1; ?>.jpg) no-repeat center center fixed',
+			'background-position' : '50% 90%',
+			'-webkit-background-size' : 'cover',
+			'-moz-background-size' : 'cover',
+			'-o-background-size' : 'cover',
+			'background-size' : 'cover',
+			'transition-duration': '.3s' 
+	    });
+	}); 
+
+
+</script>
 </body>
 </html>
