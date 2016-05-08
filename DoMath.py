ea5f8bd1b81820945db48f213815956ea3a03a6f
Kevin <kmanc@comcast.net>
2016-05-07 22:24:20 -0400

    its REALLY close...like really close to be right.  something went wrong with this most recent test, but this should get pushed to live asap because it is still a hundred times better than it was.  went from finding about 20 valid teams to about 3500

--- DoMath.py
+++ DoMath.py
@@ -9,34 +9,34 @@ class DoMath(object):
         final_champ_zero = final_champ_one = final_champ_two = final_champ_three = final_champ_four = ''
         final_role_zero = final_role_one = final_role_two = final_role_three = final_role_four = ''
         valid_roles = ['MID', 'TOP', 'JUNGLE', 'DUO_CARRY', 'DUO_SUPPORT']
+        teams=0
         
         # Find total mastery points per summoner    
         for x0, mastered_champs0 in summonerList[0].items():
             for x0, champ_info0 in mastered_champs0.items():
                 for role_info0 in champ_info0[1:]:
-                    print (champ_info0[:1], int(list(role_info0.values())[0]))
                     total_mastery0 += int(list(role_info0.values())[0])
     
         for x1, mastered_champs1 in summonerList[1].items():
             for x1, champ_info1 in mastered_champs1.items():
                 for role_info1 in champ_info1[1:]:
                     total_mastery1 += int(list(role_info1.values())[0])
-               
+       
         for x2, mastered_champs2 in summonerList[2].items():
             for x2, champ_info2 in mastered_champs2.items():
                 for role_info2 in champ_info2[1:]:
                     total_mastery2 += int(list(role_info2.values())[0])
-                
+       
         for x3, mastered_champs3 in summonerList[3].items():
             for x3, champ_info3 in mastered_champs3.items():
                 for role_info3 in champ_info3[1:]:
                     total_mastery3 += int(list(role_info3.values())[0])
-                
+        
         for x4, mastered_champs4 in summonerList[4].items():
             for x4, champ_info4 in mastered_champs4.items():
                 for role_info4 in champ_info4[1:]:
                     total_mastery4 += int(list(role_info4.values())[0])
-                    
+
         # Iterate through team combinations and find the "valid" ones (ones with each of the 5 positions)
         # Is it efficient?  Probably not, but time conquers all
         for x0, mastered_champs0 in summonerList[0].items():
@@ -46,6 +46,7 @@ class DoMath(object):
                     #print (champ_info0[:1])
                     champList = []
                     for role_info0 in champ_info0[1:]:
+                        #print (role_info0)
                         for role0 in role_info0:
                             #print ('summoner 0', x0, role0)
                             roleList = []
@@ -60,10 +61,12 @@ class DoMath(object):
                                 #print (mastered_champs1)
                                 for x1, champ_info1 in mastered_champs1.items():
                                     #print (champ_info1[1:])
+                                    champList = champList[:1]
                                     for role_info1 in champ_info1[1:]:
                                         #print (role_info1)
                                         for role1 in role_info1:
                                             #print ('summoner 1', x1, role1)
+                                            roleList = roleList[:1]
                                             summoner_one_role = role1
                                             summoner_one_champ = x1
                                             summoner_one_champ_name = champ_info1[:1]
@@ -76,10 +79,12 @@ class DoMath(object):
                                                 #print (mastered_champs2)
                                                 for x2, champ_info2 in mastered_champs2.items():
                                                     #print (champ_info2[1:])
+                                                    champList = champList[:2]
                                                     for role_info2 in champ_info2[1:]:
                                                         #print (role_info2)
                                                         for role2 in role_info2:
                                                             #print ('summoner 2', x2, role2)
+                                                            roleList = roleList[:2]
                                                             summoner_two_role = role2
                                                             summoner_two_champ = x2
                                                             summoner_two_champ_name = champ_info2[:1]
@@ -91,10 +96,12 @@ class DoMath(object):
                                                                 #print (mastered_champs3)
                                                                 for x3, champ_info3 in mastered_champs3.items():
                                                                     #print (champ_info3[1:])
+                                                                    champList = champList[:3]
                                                                     for role_info3 in champ_info3[1:]:
                                                                         #print (role_info3)
                                                                         for role3 in role_info3:
                                                                             #print ('summoner 3', x3, role3)
+                                                                            roleList = roleList[:3]
                                                                             summoner_three_role = role3
                                                                             summoner_three_champ = x3
                                                                             summoner_three_champ_name = champ_info3[:1]
@@ -106,20 +113,20 @@ class DoMath(object):
                                                                                 #print (mastered_champs4)
                                                                                 for x4, champ_info4 in mastered_champs4.items():
                                                                                     #print (champ_info4[1:])
+                                                                                    champList = champList[:4]
                                                                                     for role_info4 in champ_info4[1:]:
                                                                                         #print (role_info4)
                                                                                         for role4 in role_info4:
                                                                                             #print ('summoner 4', x4, role4)
+                                                                                            roleList = roleList[:4]
                                                                                             summoner_four_role = role4
                                                                                             summoner_four_champ = x4
                                                                                             summoner_four_champ_name = champ_info4[:1]
                                                                                             if (summoner_four_role not in valid_roles or summoner_four_role in roleList or len(roleList) <= 3 or summoner_four_champ in champList):
                                                                                                 continue
                                                                                             else:
-                                                                                                # Clean up the lists of exclusion for the next run-through
-                                                                                                roleList = []
-                                                                                                champList = []
                                                                                                 # Do math to see if the team should be played
+                                                                                                teams+=1
                                                                                                 if total_mastery0 == 0:
                                                                                                     total_mastery0 = 1
                                                                                                 if total_mastery1 == 0:
@@ -138,6 +145,7 @@ class DoMath(object):
                                                                                                 current_team_power = specific_mastery0 + specific_mastery1 + specific_mastery2 + specific_mastery3 + specific_mastery4
                                                                                                 if current_team_power > best_team_power:
                                                                                                     best_team_power = current_team_power
+                                                                                                    print (best_team_power)
                                                                                                     final_champ_zero = str(summoner_zero_champ_name).replace('[', '').replace(']', '').replace("'", "").replace('"', '')
                                                                                                     final_champ_one = str(summoner_one_champ_name).replace('[', '').replace(']', '').replace("'", "").replace('"', '')
                                                                                                     final_champ_two = str(summoner_two_champ_name).replace('[', '').replace(']', '').replace("'", "").replace('"', '')
@@ -149,6 +157,7 @@ class DoMath(object):
                                                                                                     final_role_three = str(summoner_three_role)
                                                                                                     final_role_four = str(summoner_four_role)
                                                                                             
+        print ('number of valid teams found is ', teams)
         return (summoners[0] + ',' + final_champ_zero + ',' + final_role_zero + ',' + summoners[1] + ',' + final_champ_one + ',' + final_role_one + ',' + summoners[2] + ',' + final_champ_two + ','
                              + final_role_two + ',' + summoners[3] + ',' + final_champ_three + ',' + final_role_three + ',' + summoners[4] + ',' + final_champ_four + ',' + final_role_four)     
                              
