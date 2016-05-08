04f63c31386a3ba94a0a3ac8741d8d752cf54b6d
Kevin <kmanc@comcast.net>
2016-05-08 19:17:56 -0400

    all i did was move a variable assignment so that it was not in the comment block saying it would be dealt with later

--- Main.py
+++ Main.py
@@ -12,9 +12,10 @@ def main():
     except:
         sys.exit('At least one summoner name was not entered')
         
-    # These will likely get moved to a user-entered variable instead of hard coded
-    champs = 10
     region = sys.argv[6]
+    # These will likely get moved to a user-entered variable instead of hard coded
+    #champs = sys.argv[7]
+    champs = 10
     
     # Normalize summoner names (no spaces, no upper case) because the api can get cranky if you don't
     for x in range(len(summoners)):
