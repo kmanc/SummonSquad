275237bfcb278cdf74dba78b0f85f092bdbfd413
Kevin <kmanc@comcast.net>
2017-06-12 09:55:41 -0700

    not sure what i did here but this needs an overhaul anyway

--- RiotAPI.py
+++ RiotAPI.py
@@ -5,43 +5,45 @@ import requests
 class RiotAPI(object):
     
     number = 0
+
     def __init__(self, region):
         self.api_key = KeyAPI.key
-        if (region == 'NA'):
+
+        if region == 'NA':
             self.region = Constants.REGIONS['north_america']
             self.platform = Constants.PLATFORMS['north_america']
-        elif (region == 'BR'):
+        elif region == 'BR':
             self.region = Constants.REGIONS['brazil']
             self.platform = Constants.PLATFORMS['brazil']
-        elif (region == 'EUNE'):
+        elif region == 'EUNE':
             self.region = Constants.REGIONS['eu_nordic']
             self.platform = Constants.PLATFORMS['eu_nordic']
-        elif (region == 'EUW'):
+        elif region == 'EUW':
             self.region = Constants.REGIONS['eu_west']
             self.platform = Constants.PLATFORMS['eu_west']
-        elif (region == 'JP'):
+        elif region == 'JP':
             self.region = Constants.REGIONS['japan']
             self.platform = Constants.PLATFORMS['japan']
-        elif (region == 'KR'):
+        elif region == 'KR':
             self.region = Constants.REGIONS['korea']
             self.platform = Constants.PLATFORMS['korea']
-        elif (region == 'LAN'):
+        elif region == 'LAN':
             self.region = Constants.REGIONS['latin_america_north']
             self.platform = Constants.PLATFORMS['latin_america_north']
-        elif (region == 'LAS'):
+        elif region == 'LAS':
             self.region = Constants.REGIONS['latin_america_south']
             self.platform = Constants.PLATFORMS['latin_america_south']
-        elif (region == 'OCE'):
+        elif region == 'OCE':
             self.region = Constants.REGIONS['oceania']
             self.platform = Constants.PLATFORMS['oceania']
-        elif (region == 'TR'):
+        elif region == 'TR':
             self.region = Constants.REGIONS['turkey']
             self.platform = Constants.PLATFORMS['turkey']
-        elif (region == 'RU'):
+        elif region == 'RU':
             self.region = Constants.REGIONS['russia']
             self.platform = Constants.PLATFORMS['russia']
         
-    def _request_summoner(self, api_url, params={}):
+    def request_summoner(self, api_url, params={}):
         args = {'api_key': self.api_key}
         for key, value in params.items():
             if key not in args:
@@ -56,7 +58,7 @@ class RiotAPI(object):
         )
         return response.json()
         
-    def _request_mastery(self, api_url, params={}):
+    def request_mastery(self, api_url, params={}):
         args = {'api_key': self.api_key}
         for key, value in params.items():
             if key not in args:
@@ -70,8 +72,7 @@ class RiotAPI(object):
         )
         return response.json()
         
-    def _request_top_mastery(self, api_url, params={}):
-        args = params
+    def request_top_mastery(self, api_url, params={}):
         args = {'api_key': self.api_key}
         for key, value in params.items():
             if key not in args:
@@ -85,8 +86,7 @@ class RiotAPI(object):
         )
         return response.json()
         
-    def _request_champion_role(self, api_url, params={}):
-        args = params
+    def request_champion_role(self, api_url, params={}):
         args = {'api_key': self.api_key}
         for key, value in params.items():
             if key not in args:
@@ -101,7 +101,7 @@ class RiotAPI(object):
         )
         return response.json()
         
-    def _request_champion_name(self, champIdizzle):
+    def request_champion_name(self, champIdizzle):
         args = {'api_key': self.api_key}
         response = requests.get(
             Constants.URL['global_base'].format(
@@ -119,7 +119,7 @@ class RiotAPI(object):
             version=Constants.API_VERSIONS['summoner'],
             names=name
         )
-        return self._request_summoner(api_url)
+        return self.request_summoner(api_url)
        
     def get_summoner_mastery_data(self, id):
         # Get all summoner mastery data
@@ -127,7 +127,7 @@ class RiotAPI(object):
             platformId=self.platform,
             playerId=id
         )
-        return self._request_mastery(api_url)
+        return self.request_mastery(api_url)
         
     def get_top_mastery_data(self, id, champs):
         # Get mastery data for the top X champions, where X is the length of the list 'champs'
@@ -135,7 +135,7 @@ class RiotAPI(object):
             platformId=self.platform,
             playerId=id
         )
-        return self._request_top_mastery(api_url, {'count': champs})
+        return self.request_top_mastery(api_url, {'count': champs})
         
     def get_champion_role(self, id, champList):
         # Figure out which roles the champion is being played in based off of ranked games
@@ -145,11 +145,11 @@ class RiotAPI(object):
         )
         queues = 'TEAM_BUILDER_DRAFT_RANKED_5x5,RANKED_SOLO_5x5,RANKED_TEAM_5x5'
         champList = ','.join(map(str, champList))
-        return self._request_champion_role(api_url, {'championIds': champList, 'rankedQueues': queues})
+        return self.request_champion_role(api_url, {'championIds': champList, 'rankedQueues': queues})
         
     def get_champion_name(self, champIdizzle):
         # Get champion name from champion ID so we can tell a player they should play 'Thresh', not 'champion 412'
-        return self._request_champion_name(champIdizzle)
+        return self.request_champion_name(champIdizzle)
                
 # If request isn't working, check "print (response.url)" first!!!
         
