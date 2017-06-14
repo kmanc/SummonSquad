79e03b3ff0e70ba99e1547cc8eba1b6f1cb3cf0f
Kevin <kmanc@comcast.net>
2017-06-13 19:24:27 -0700

    switching to api v3 constants

--- APIConstants.py
+++ APIConstants.py
@@ -1,46 +1,24 @@
 URL = {
-    'base': 'https://{proxy}.api.pvp.net/api/lol/{region}/{url}',
-    'summoner_by_name':'v{version}/summoner/by-name/{names}',
-    'mastery_base':'https://{proxy}.api.pvp.net/{url}',
-    'all_mastery':'championmastery/location/{platformId}/player/{playerId}/champions',
-    'top_x_mastery':'championmastery/location/{platformId}/player/{playerId}/topchampions',
-    'mastery_by_champ':'championmastery/location/{platformId}/player/{playerId}/champion/{championId}',
-    'matchlist':'v{version}/matchlist/by-summoner/{playerId}',
-    'global_base':'https://global.api.pvp.net/api/lol/static-data/{region}/v{version}/champion/{championId}'
+    'base': 'https://{region}.api.riotgames.com/{asset}',
 }
 
-API_VERSIONS = {
-    'champion': '1.2',
-    'matchlist': '2.2',
-    'stats': '1.3',
-    'summoner': '1.4',
-    'global': '1.2'
+ASSETS = {
+    'champion_mastery': '/lol/champion-mastery/v{version}/champion-masteries/by-summoner/{summonerId}',
+    'summoner_lookup': '/lol/summoner/v{version}/summoners/by-name/{summonerName}',
+    'match_history': '/lol/match/v{version}/matchlists/by-account/{accountId}',
+    'champion_lookup': '/lol/static-data/v{version}/champions',
 }
 
 REGIONS = {
-    'north_america': 'na',
-    'brazil': 'br',
-    'eu_nordic': 'eune',
-    'eu_west': 'euw',
-    'japan': 'jp',
-    'korea': 'kr',
-    'latin_america_north': 'lan',
-    'latin_america_south': 'las',
-    'oceania': 'oce',
-    'turkey': 'tr',
-    'russia': 'ru'
-}
-
-PLATFORMS = {
-    'north_america': 'NA1',
-    'brazil': 'BR1',
-    'eu_nordic': 'EUN1',
-    'eu_west': 'EUW1',
-    'japan': 'JP1',
-    'korea': 'KR',
-    'latin_america_north': 'LA1',
-    'latin_america_south': 'LA2',
-    'oceania': 'OC1',
-    'turkey': 'TR1',
-    'russia': 'RU'
+    'NA': 'na1',
+    'BR': 'br1',
+    'EUN': 'eun1',
+    'EUW': 'euw1',
+    'JP': 'jp1',
+    'KR': 'kr',
+    'LAN': 'la1',
+    'LAS': 'la2',
+    'OC': 'oc1',
+    'TR': 'tr1',
+    'RU': 'ru'
 }
