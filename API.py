4b61b4351dfb70c6b61aadbec190e43fab3cf131
Kevin <kmanc@comcast.net>
2017-06-13 19:50:45 -0700

    API v3 calls

--- API.py
+++ API.py
@@ -1,5 +1,4 @@
-import NewAPIConstants as Constants
-import KeyAPI
+import APIConstants, KeyAPI
 import requests
 
 api_key = KeyAPI.key
@@ -7,35 +6,35 @@ args = {'api_key': api_key}
 
 
 def get_region(region):
-    return Constants.REGIONS[region]
+    return APIConstants.REGIONS[region]
 
 
 def summoner_lookup(summoner_name, region):
     region = get_region(region)
-    asset = Constants.ASSETS['summoner_lookup'].format(version=3, summonerName=summoner_name)
-    url = Constants.URL['base'].format(region=region, asset=asset)
+    asset = APIConstants.ASSETS['summoner_lookup'].format(version=3, summonerName=summoner_name)
+    url = APIConstants.URL['base'].format(region=region, asset=asset)
     return requests.get(url, params=args).json()
 
 
-def champion_mastery(summoner_id, region, num_champs):
+def champion_mastery(summoner_id, num_champs, region):
     region = get_region(region)
-    asset = Constants.ASSETS['champion_mastery'].format(version=3, summonerId=summoner_id)
-    url = Constants.URL['base'].format(region=region, asset=asset)
+    asset = APIConstants.ASSETS['champion_mastery'].format(version=3, summonerId=summoner_id)
+    url = APIConstants.URL['base'].format(region=region, asset=asset)
     return (requests.get(url, params=args).json())[:num_champs]
 
 
-def match_history(account_id, region, champ_list):
+def match_history(account_id, champ_list, region):
     region = get_region(region)
     args['champion'] = champ_list
-    asset = Constants.ASSETS['match_history'].format(version=3, accountId=account_id)
-    url = Constants.URL['base'].format(region=region, asset=asset)
+    asset = APIConstants.ASSETS['match_history'].format(version=3, accountId=account_id)
+    url = APIConstants.URL['base'].format(region=region, asset=asset)
     return requests.get(url, params=args).json()
 
 
 def champion_lookup(region):
     region = get_region(region)
     args['dataById'] = 'true'
-    asset = Constants.ASSETS['champion_lookup'].format(version=3)
-    url = Constants.URL['base'].format(region=region, asset=asset)
+    asset = APIConstants.ASSETS['champion_lookup'].format(version=3)
+    url = APIConstants.URL['base'].format(region=region, asset=asset)
     result = requests.get(url, params=args).json()
     return result['data']
