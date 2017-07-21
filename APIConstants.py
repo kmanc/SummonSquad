c22a02bcbfaaa82f51c773ed974d61fe7164d3af
Kevin <kmanc@comcast.net>
2017-07-20 19:22:37 -0700

    fixed a non-impacting bug where the api links were not properly formatted

--- APIConstants.py
+++ APIConstants.py
@@ -3,10 +3,10 @@ URL = {
 }
 
 ASSETS = {
-    'champion_mastery': '/lol/champion-mastery/v{version}/champion-masteries/by-summoner/{summonerId}',
-    'summoner_lookup': '/lol/summoner/v{version}/summoners/by-name/{summonerName}',
-    'match_history': '/lol/match/v{version}/matchlists/by-account/{accountId}',
-    'champion_lookup': '/lol/static-data/v{version}/champions',
+    'champion_mastery': 'lol/champion-mastery/v{version}/champion-masteries/by-summoner/{summonerId}',
+    'summoner_lookup': 'lol/summoner/v{version}/summoners/by-name/{summonerName}',
+    'match_history': 'lol/match/v{version}/matchlists/by-account/{accountId}',
+    'champion_lookup': 'lol/static-data/v{version}/champions',
 }
 
 REGIONS = {
