bafbfce598e881126505c11f1e4c4e405adb5bd2
Kevin <kmanc@comcast.net>
2017-08-01 20:54:10 -0700

    script that updates champion dictionary that will be run via cron

---
+++ update_champs.py
@@ -0,0 +1,15 @@
+import api_constants, api_key
+import requests, os, json
+
+region = 'na1'
+api_key = api_key.key
+args = {'api_key': api_key}
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+champ_file = '{0}/{1}'.format(dir_path, 'champion_dict.py')
+args['dataById'] = 'true'
+asset = api_constants.ASSETS['champion_lookup'].format(version=3)
+url = api_constants.URL['base'].format(region=region, asset=asset)
+result = requests.get(url, params=args).json()
+with open(champ_file, 'w') as f:
+    f.write(json.dumps(result['data']))
