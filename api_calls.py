6e8749b44c4ad2c4b9515a40b742c67ab10af8da
Kevin <kmanc@comcast.net>
2017-08-01 20:52:06 -0700

    reformatting for future changes

--- api_calls.py
+++ api_calls.py
@@ -13,14 +13,16 @@ def summoner_lookup(summoner_name, region):
     region = get_region(region)
     asset = api_constants.ASSETS['summoner_lookup'].format(version=3, summonerName=summoner_name)
     url = api_constants.URL['base'].format(region=region, asset=asset)
-    return requests.get(url, params=args).json()
+    result = requests.get(url, params=args).json()
+    return result
 
 
 def champion_mastery(summoner_id, num_champs, region):
     region = get_region(region)
     asset = api_constants.ASSETS['champion_mastery'].format(version=3, summonerId=summoner_id)
     url = api_constants.URL['base'].format(region=region, asset=asset)
-    return (requests.get(url, params=args).json())[:num_champs]
+    result = requests.get(url, params=args).json()
+    return result[:num_champs]
 
 
 def match_history(account_id, champ_list, region):
@@ -28,7 +30,8 @@ def match_history(account_id, champ_list, region):
     args['champion'] = champ_list
     asset = api_constants.ASSETS['match_history'].format(version=3, accountId=account_id)
     url = api_constants.URL['base'].format(region=region, asset=asset)
-    return requests.get(url, params=args).json()
+    result = requests.get(url, params=args).json()
+    return result
 
 
 def champion_lookup(region):
